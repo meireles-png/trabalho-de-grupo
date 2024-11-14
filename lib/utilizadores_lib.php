7591d799f7c7585fc803361db593188953204ce5
Alexandre <alexandrefonsecafelgueiras@gmail.com>
2024-11-14 09:35:03 -0100

    cookies

--- lib/utilizadores_lib.php
+++ lib/utilizadores_lib.php
@@ -4,29 +4,40 @@ function validaUtilizador(string $username, string $password): array|bool
 {
     // abrir o ficheiro no directorio superior data/utilizadores
 
-    $futilizadores = fopen('data' . DIRECTORY_SEPARATOR . 'utilizadores.txt', 'r');
+    $futilizadores = fopen(
+        "data"
+        . DIRECTORY_SEPARATOR
+        . "utilizadores.txt",
+    "r"
+);
+while(($linha = fgets($futilizadores)) !== false) {
+    $utilizador = explode(",", $linha);
 
-    while(($linha = fgets($futilizadores)) !== false) {
-        $utilizador = explode(",", $linha);
-
-        if ($username == $utilizador[0]) {
-            if (password_verify($password, $utilizador[1])) {
-                session_start();
-                $_SESSION['nome'] = $utilizador[2];
-                return $utilizador;
-            } else {
-                return false;
-            }
+    if ($username == $utilizador[0]) {
+        if (password_verify($password, $utilizador[1])) {
+            session_start();
+            $_SESSION['nome'] = $utilizador[2];
+            setcookie('sócioslogin', json_encode);
+            return $utilizador;
+        } else {
+            return false;
         }
     }
+}
 
-    return false;
+return false;
 }
 
 function validaSessao(): bool
 {
     session_start();
     if (empty($_SESSION) || empty($_SESSION['nome'])) {
+        if (isset($_COOKIE['sócioslogin'])) {
+            $dadosdacookie = json_decode($_COOKIE['sócioslogin'], true);
+            return validaUtilizador($dadoscookies['utilizadores'], $dadoscookies['password']);
+        } else {
+            return false;
+        }
         return false;
     }
 
@@ -38,6 +49,8 @@ function terminaSessao(): bool
     if (!validaSessao()) {
         return true;
     } 
+
+    setcookie('sócioslogin', '', time()-1);
 
     $_SESSION = [];
     session_destroy();
